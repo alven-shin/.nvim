5a71928b5371f5c13bf2d8003fdef02c65be9ba5
Alvin Shen <29553973+alven-shin@users.noreply.github.com>
2025-12-20 10:09:12 -0500

    feat: hide messages

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -73,3 +73,5 @@ vim.opt.tabstop = 4 -- number of visual spaces per TAB
 -- vim.opt.softtabstop = 4 -- number of spaces in tab when editing
 vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of Python
+
+vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
