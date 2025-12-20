737f27fdfc918f0d9a171e97da9493db471951ea
Alvin Shen <29553973+alven-shin@users.noreply.github.com>
2025-12-19 20:32:11 -0500

    feat: update autopair config
    copied config from lazyvim

--- lua/user/autopair.lua
+++ lua/user/autopair.lua
@@ -1,4 +1,15 @@
-require("mini.pairs").setup()
+require("mini.pairs").setup({
+  modes = { insert = true, command = true, terminal = false },
+  -- skip autopair when next character is one of these
+  skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+  -- skip autopair when the cursor is inside these treesitter nodes
+  skip_ts = { "string" },
+  -- skip autopair when next character is closing pair
+  -- and there are more closing pairs than opening pairs
+  skip_unbalanced = true,
+  -- better deal with markdown code blocks
+  markdown = true,
+})
 require("nvim-ts-autotag").setup()
 
 -- NOTE: autopair can be disabled by setting a buffer specific keymap in an ftplugin
