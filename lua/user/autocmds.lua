7b5653b7c45f6127eddec3cf8507c59202fab450
Alvin Shen <29553973+alven-shin@users.noreply.github.com>
2025-12-20 12:57:41 -0500

    feat: save folds and cursor position

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -12,3 +12,21 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<cmd>quit<CR>", { buffer = true })
   end,
 })
+
+-- save folds and cursor position
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype ~= "" and vim.bo.buftype == "" then
+      vim.cmd("mkview")
+    end
+  end,
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype ~= "" and vim.bo.buftype == "" then
+      vim.cmd("loadview")
+    end
+  end,
+})
